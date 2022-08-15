d0d19618052dc1994cf3803aa52e3e5ced1470d7
mannnish <mk458978@gmail.com>
2022-08-15 15:53:38 +0530

    remodelled

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # my-cli
 
 1. Installation : `sudo i -g t0ddu`
-1. Run : `my-cli` or `desk`
+1. Config : `desk config` and enter key
+1. Run : `desk`
--- index.js
+++ index.js
@@ -1,76 +1,131 @@
 #!/usr/bin/env node
-
-/**
- * my-cli
- * this is test
- *
- * @author mannnish <https://mannnish.vercel.app>
- */
-
+const inquirer = require('inquirer');
+const fetch = require('node-fetch')
+const cheerio = require('cheerio');
+const fs = require('fs');
 const init = require('./utils/init');
 const cli = require('./utils/cli');
-const log = require('./utils/log');
-const inquirer = require('inquirer');
-const chalk = require('chalk')
-
+const chalk = require('chalk');
 const input = cli.input;
-const flags = cli.flags;
-const { clear, debug } = flags;
 
-const tasks = [
-	{
-		"title": "5 questions from DSA Sheet",
-		"function": dsaSheet
-	},
-	{
-		"title": "any zattire work if left by chance",
-		"function": zattireFn,
-	},
-	{
-		"title": "interview prep",
-		"function": zattireFn,
-	},
-	{
-		"title": "gym",
-		"function": gymFn
+authMiddleware = async () => {
+	const existingConfig = fs.existsSync('./config.json');
+	if (!existingConfig) {
+		console.log(chalk.red('Authentication Error'));
+		return false;
+	} else {
+		const config = require('./config.json');
+		if (!config.key) {
+			console.log(chalk.red('Authentication Error'));
+			return false;
+		}
+		return await checkForKey(config.key);
 	}
-];
+};
 
+checkForKey = async (token) => {
+	const url = `https://api.todoist.com/rest/v1/labels`;
+	const headers = { 'Authorization': `Bearer ${token}` };
+	const response = await fetch(url, { headers });
+	if (response.status == 200) return true;
+	else return false;
+}
 
-(async () => {
-	init({ clear });
-	debug && log(flags);
+fetchByteBlog = async () => {
+	console.log(chalk.bgYellow('tech-blog  : '));
+	const response = await fetch('https://blog.bytebytego.com/');
+	const body = await response.text();
+	const $ = cheerio.load(body);
 
-	console.log(chalk.bgBlue(' tasks: '));
-	tasks.forEach((element, index) => {
-		console.log(`${index + 1}. ${element.title}`);
+	let title, description;
+	$('.home-big-post a.post-preview-title').each((i, data) => {
+		const titleNode = $(data);
+		title = titleNode.text();
 	});
-	console.log('\n');
-	const option = await inquirer.prompt({
-		name: 'task_number',
-		type: 'input',
-		message: 'Which task do you want to do?'
+	$('.home-big-post a.post-preview-description').each((i, data) => {
+		const titleNode = $(data);
+		description = titleNode.text();
+	});
+	console.log(title);
+	console.log(chalk.grey(description));
+	console.log('\n')
+}
+
+fetchTodoistTaks = async () => {
+	console.log(chalk.bgRedBright('todoist tasks : '));
+	const config = require('./config.json');
+	const query = `?filter=today`
+	const url = `https://api.todoist.com/rest/v1/tasks/${query}`;
+	const headers = {
+		'Authorization': `Bearer ${config.key}`
+	};
+	const response = await fetch(url, { headers });
+	const data = await response.json();
+	let list = [];
+	data.forEach(item => {
+		console.log(item.content);
 	})
-	console.log(`\n${chalk.bgGreenBright(` processing `)} : ${tasks[option.task_number - 1].title}\n`);
-	console.log(tasks[option.task_number - 1].function());
-	process.exit(0);
-})();
+	console.log('\n');
+}
 
 function dsaSheet() {
 	const max = 479;
 	const min = 4;
 	let list = [];
-	// for loop 1 to 5
 	for (let i = 0; i < 5; i++) {
-		list.push(Math.floor(Math.random() * (max - min) + min))
+		list.push(Math.floor(Math.random() * (max - min) + min));
 	}
-	return list;
-}
-
-function gymFn() {
-	return "happy gym"
+	console.log(list);
 }
 
 function zattireFn() {
-	return "happy zattire"
+	console.log('happy zattire');
 }
+
+const tasks = [
+	{ title: '5 questions from DSA Sheet', function: dsaSheet },
+	{ title: 'interview prep', function: zattireFn },
+	{ title: 'todoist add task for today', function: addTask },
+	{ title: 'update self-note', function: updateSelfNote }
+];
+
+(async () => {
+	init({ clear: true });
+	if (input.includes('config')) {
+		const key = await inquirer.prompt({
+			name: 'key',
+			message: 'Enter your api key',
+			type: 'input'
+		});
+		fs.writeFileSync(
+			'./config.json',
+			JSON.stringify({ key: key.key })
+		);
+		if (await checkForKey(key.key)) console.log(chalk.greenBright('Authentication Successful'));
+		else console.log(chalk.red('Authentication Error'));
+
+
+	} else if (input.includes('logout')) {
+		const existingConfig = fs.existsSync('./config.json');
+		if (!existingConfig) console.log(chalk.red('No config'));
+		else fs.unlinkSync('./config.json')
+
+
+	} else {
+		// const login = await authMiddleware();
+		// if (!login) return
+
+		// await fetchTodoistTaks();
+		// await fetchByteBlog();
+		const input = await inquirer.prompt({
+			type: 'list',
+			name: 'task',
+			message: 'Select a task',
+			choices: tasks.map(task => task.title)
+		})
+		console.log(input.task);
+		const selectedTask = tasks.find(task => task.title === input.task);
+		selectedTask.function();
+	}
+	process.exit(0);
+})();
