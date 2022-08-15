7084558e5702c5a6ef42c56741e39322fac3d587
mannnish <mk458978@gmail.com>
2022-08-15 20:51:41 +0530

    modify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fetch = require('node-fetch')
 const cheerio = require('cheerio');
+const { exec } = require('child_process');
 const fs = require('fs');
 const init = require('./utils/init');
 const cli = require('./utils/cli');
@@ -15,11 +16,11 @@ authMiddleware = async () => {
 		return false;
 	} else {
 		const config = require('./config.json');
-		if (!config.key) {
+		if (!config.todoist_key) {
 			console.log(chalk.red('Authentication Error'));
 			return false;
 		}
-		return await checkForKey(config.key);
+		return await checkForKey(config.todoist_key);
 	}
 };
 
@@ -57,11 +58,10 @@ fetchTodoistTaks = async () => {
 	const query = `?filter=today`
 	const url = `https://api.todoist.com/rest/v1/tasks/${query}`;
 	const headers = {
-		'Authorization': `Bearer ${config.key}`
+		'Authorization': `Bearer ${config.todoist_key}`
 	};
 	const response = await fetch(url, { headers });
 	const data = await response.json();
-	let list = [];
 	data.forEach(item => {
 		console.log(item.content);
 	})
@@ -76,47 +76,82 @@ function dsaSheet() {
 		list.push(Math.floor(Math.random() * (max - min) + min));
 	}
 	console.log(list);
+	exec(`google-chrome https://docs.google.com/spreadsheets/d/1ryH86xZ_m2zvuAw0dZqlMHQVJROPqX7p/edit#gid=866777697`)
 }
 
-function zattireFn() {
-	console.log('happy zattire');
+function interviewFn() {
+	const config = require('./config.json');
+	const url = config.interview_prep
+	exec(`google-chrome ${url}`)
 }
 
 const tasks = [
 	{ title: '5 questions from DSA Sheet', function: dsaSheet },
-	{ title: 'interview prep', function: zattireFn },
-	{ title: 'todoist add task for today', function: addTask },
-	{ title: 'update self-note', function: updateSelfNote }
+	{ title: 'interview prep', function: interviewFn },
+	// { title: 'todoist add task for today', function: addTask },
+	// { title: 'update self-note', function: updateSelfNote }
 ];
 
 (async () => {
 	init({ clear: true });
-	if (input.includes('config')) {
-		const key = await inquirer.prompt({
-			name: 'key',
-			message: 'Enter your api key',
-			type: 'input'
-		});
+	if (input.includes('config-set')) {
+		let defTodoist, defAccess;
+		try {
+			const config = require('./config.json');
+			defTodoist = config.todoist_key;
+			defAccess = config.access_token;
+		} catch (error) { }
+
+		const key = await inquirer.prompt([
+			{
+				name: 'todoist_key',
+				message: 'Enter your api key',
+				type: 'input',
+				default: defTodoist
+			},
+			{
+				name: 'interview_prep',
+				message: 'Enter your interview prep url',
+				type: 'input',
+				default: 'https://www.notion.so/Interview-based-81298d3f66824cfcbc56a205bf306f00#86c0f1889dbf4437a97b48321146162c'
+			},
+			{
+				name: 'access_token',
+				message: 'Enter your access token',
+				type: 'input',
+				default: defAccess
+			},
+		]);
 		fs.writeFileSync(
 			'./config.json',
-			JSON.stringify({ key: key.key })
+			JSON.stringify(key)
 		);
-		if (await checkForKey(key.key)) console.log(chalk.greenBright('Authentication Successful'));
+		if (await checkForKey(key.todoist_key)) console.log(chalk.greenBright('Authentication Successful'));
 		else console.log(chalk.red('Authentication Error'));
 
 
-	} else if (input.includes('logout')) {
+	} else if (input.includes('config-remove')) {
 		const existingConfig = fs.existsSync('./config.json');
 		if (!existingConfig) console.log(chalk.red('No config'));
 		else fs.unlinkSync('./config.json')
 
 
-	} else {
-		// const login = await authMiddleware();
-		// if (!login) return
+	} else if (input.includes('config-get')) {
+		const existingConfig = fs.existsSync('./config.json');
+		if (!existingConfig) console.log(chalk.red('No config'));
+		else {
+			const config = require('./config.json');
+			console.log(config)
+		}
 
-		// await fetchTodoistTaks();
-		// await fetchByteBlog();
+
+	}
+	else {
+		const login = await authMiddleware();
+		if (!login) return
+
+		await fetchTodoistTaks();
+		await fetchByteBlog();
 		const input = await inquirer.prompt({
 			type: 'list',
 			name: 'task',
